08f87b3ed6e61b7fb0108a18472e34adae263f41
StarkOne <Stark.pro40@gmail.com>
2019-05-02 06:56:09 +0700

    func refartor nodejs

--- notes.js
+++ notes.js
@@ -4,8 +4,6 @@ const command = process.argv[2];
 const title = process.argv[3];
 const content = process.argv[4];
 
-console.log(command, title, content);
-
 switch (command) {
   case 'list':
     list((error, notes) => {
@@ -49,55 +47,69 @@ function checkFileExists() {
   }); 
 }
 
+function load(done) {
+  fs.readFile("notes.json", "utf8", function (error, data) {
+    if (error) {
+      if (error.code === "ENOENT") {
+        return done(null, []);
+      } else {
+        return done(error);
+      }
+    };
+    try {
+      const notes = JSON.parse(data);
+      done(null, notes);
+    } catch (error) {
+      done(new Error("Не удалось преобразовать данные."));
+    }
+  });
+}
+
+function save(notes, done) {
+  try {
+      const json = JSON.stringify(notes);
+      fs.writeFile('notes.json', json, error => {
+        if (error) return done(error);
+        done();
+      })
+  } catch (error) {
+    done(error);
+  }
+}
+
 function list(done) {
   checkFileExists();
-  fs.readFile("notes.json", "utf8", function (error, data) {
-    if (error) return done(error);
-    const notes = JSON.parse(data);
-    done(null, notes);
-  });
+  load(done);
 }
 
 function view(title, done) {
   checkFileExists();
-  fs.readFile("notes.json", "utf8", function (error, data) {
+  load((error, notes) => {
     if (error) return done(error);
-    const notes = JSON.parse(data);
     const note = notes.find(note => {
-      return note.title = title
+      return note.title == title
     })
     if (!note) {
       return done(new Error('Заметка не найдена'))
     }
     done(null, note);
-  });
+  })
 }
 
 function create(title, content, done) {
   checkFileExists();
-  fs.readFile("notes.json", "utf8", function (error, data) {
+  load((error, notes) => {
     if (error) return done(error);
-    const notes = JSON.parse(data);
-    notes.push({title, content});
-    const json = JSON.stringify(notes);
-    fs.writeFile('notes.json', json, error => {
-      if (error) return done(error);
-
-      done();
-    })
+    notes.push({ title, content });
+    save(notes, done);
   });
 }
 
 function remove(title, done) {
   checkFileExists();
-  fs.readFile("notes.json", "utf8", function (error, data) {
+  load((error, notes) => {
     if (error) return done(error);
-    let notes = JSON.parse(data);
     notes = notes.filter(note => note.title !== title);
-    const json = JSON.stringify(notes);
-    fs.writeFile('notes.json', json, error => {
-      if (error) return done(error);
-      done();
-    })
-  });
+    save(notes, done);
+  })
 }
